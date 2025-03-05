e6fab2842e206e21399f0b365397e5fa2447b84c
jk280280 <38111277+jk280280@users.noreply.github.com>
2025-03-05 16:13:06 +0530

    Create iam.tf

---
+++ modules/eks/iam.tf
@@ -0,0 +1,36 @@
+resource "aws_iam_role" "eks" {
+  name = "eksServiceRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = ["eks.amazonaws.com", "ec2.amazonaws.com"]
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks" {
+  role       = aws_iam_role.eks.name
+  policy_arn  = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node" {
+  role       = aws_iam_role.eks.name
+  policy_arn  = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2" {
+  role       = aws_iam_role.eks.name
+  policy_arn  = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+# Attach the necessary VPC CNI permissions to the EKS worker node role
+resource "aws_iam_role_policy_attachment" "vpc_cni" {
+  role       = aws_iam_role.eks.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"  # Required for VPC CNI plugin
+}
